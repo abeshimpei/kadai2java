68d35ea59ed9da4fed1fe8f7714dafc98b461cc3
EC2 Default User <ec2-user@ip-172-31-10-124.ec2.internal>
2022-03-07 08:04:08 +0000

    電卓課題提出

--- main.js
+++ main.js
@@ -1,66 +1,20 @@
-let time = document.getElementById('time')
-let start = document.getElementById('start')
-let stop = document.getElementById('stop')
-let reset = document.getElementById('reset')
-
-let startTime;
-let elapsed = 0; 
-let timeId;
-let timeTodd = 0;
-
-function updateTimeText(){
-
-  let ms = elapsed % 1000;
-  let s = Math.floor(elapsed % 60000 / 1000);
-  let m = Math.floor(elapsed / 60000);
-  let h = Math.floor(elapsed / 360000);
-  
-  let msStr = ms.toString().slice(0,1);
- 
-  
-  time.textContent = h + ':' + m + ':' + s + ':' + msStr;
- 
-};
-
-function countUp(){
-
-  timeId = setTimeout(function(){
+function clickbutton(target){
+    let result = document.getElementById("result")
+    let target_value = target.innerHTML;
     
-    elapsed = Date.now() - startTime + timeTodd;
-    updateTimeText();
-    countUp();
-  }, 10);
-}
-
-start.addEventListener('click', function(){
- 
-  start.disabled = true;
-  stop.disabled = false;
-  reset.disabled = false;
-  
-  startTime = Date.now();
-  countUp();
-  
-});
-
-stop.addEventListener('click', function(){
-  
-  start.disabled = false;
-  stop.disabled = true;
-  
-  clearTimeout(timeId);
-  timeTodd += Date.now() - startTime;
-  
-});
-
-reset.addEventListener('click', function(){
-  
-  start.disabled = false;
-  reset.disabled = true;
-  
-  clearTimeout(timeId);
-  elapsed = 0;
-  timeTodd =0;
-  updateTimeText();
-  
-});
+    if (target_value == "AC") {
+       result.innerHTML = "0"
+    } else if (target_value == "=") {
+        result.innerHTML = eval(result.innerHTML)
+    } else {
+        if (result.innerHTML == "0") {
+            result.innerHTML = target_value
+        } else if (result.innerHTML == "00") {
+           result.innerHTML = target_value 
+        } else {
+            result.innerHTML += target_value
+            
+        }
+    }    
+    
+};
